fc958de31d97302c484df5d6d316d8f7bb0b9076
ngocanhInfoplus <ngocanh@infoplusvn.com>
2020-07-02 10:48:31 +0700

    Commit

---
+++ src/main/java/infostaff/Infostaff2020Application.java
@@ -0,0 +1,13 @@
+package infostaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Infostaff2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Infostaff2020Application.class, args);
+	}
+
+}
