87a719521d9108ed15ee12a5eb440b0de2da97a1
ngocanhInfoplus <ngocanh@infoplusvn.com>
2020-08-07 16:19:48 +0700

    s

--- src/main/java/infostaff/config/WebConfig.java
+++ src/main/java/infostaff/config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
 		
 		registry.addMapping("/**")
 		.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-		.allowedOrigins("http://localhost:4200")
+		.allowedOrigins("*")
 		.allowedHeaders("Authorization", "Cache-Control", "Content-Type", "Accept", "X-Requested-With",
 				"Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin")
 		.allowedHeaders("Access-Control-Expose-Headers", "Authorization", "Cache-Control", "Content-Type",
