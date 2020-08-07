0105f6146f5f5cd5e0442543ab2dc54fbc11bc17
BigKwan <chomchomcapital@gmail.com>
2020-08-07 16:26:30 +0700

    Update cibuild.sh
    Update cibuild.sh

--- cibuild.sh
+++ cibuild.sh
@@ -2,6 +2,8 @@
 
 ssh -i deploy_rsa root@35.197.156.121 <<EOF
   pkill -f "java -jar /home/infostaff/java/infostaff-0.0.1.jar"
+  pkill -f "java -jar infostaff-0.0.1.jar"
+  sleep 1
   nohup java -jar /home/infostaff/java/infostaff-0.0.1.jar > /dev/null &
   echo "DONE"
 EOF
