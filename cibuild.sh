b00dcf2d769c8bc582e1ca10396e282314f7156c
quandev.com <chomchomcapital@gmail.com>
2020-08-17 13:33:15 +0700

    update cd/ci

--- cibuild.sh
+++ cibuild.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 sshpass -p "lilama1996" ssh -t  -o StrictHostKeyChecking=no root@104.199.242.108 <<EOF
+  echo "START INSTALL"
   pkill -f "java -jar /home/infostaff/java/infostaff-0.0.1.jar"
   pkill -f "java -jar infostaff-0.0.1.jar"
-  sleep 1
+#  sleep 1
   nohup java -jar /home/infostaff/java/infostaff-0.0.1.jar > /dev/null 2>&1 &
-  echo "DONE"
+  echo "INSTALL DONE"
 EOF
