5a62b9018e89d2876e40d200806e040294edcf83
quandev.com <chomchomcapital@gmail.com>
2020-08-17 11:49:24 +0700

    update cd/ci

--- cibuild.sh
+++ cibuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sshpass -p "lilama1996" ssh root@104.199.242.108 'bash -s' <<EOF
+sshpass -p "lilama1996" ssh root@104.199.242.108 <<EOF
   pkill -f "java -jar /home/infostaff/java/infostaff-0.0.1.jar"
   pkill -f "java -jar infostaff-0.0.1.jar"
   sleep 1
