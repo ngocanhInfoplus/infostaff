1cc38bce1d52dacb452d80d75beda2523006e1e4
quandev.com <chomchomcapital@gmail.com>
2020-08-17 13:27:02 +0700

    update cd/ci

--- cibuild.sh
+++ cibuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sshpass -p "lilama1996" ssh -t -t -o StrictHostKeyChecking=no root@104.199.242.108 <<EOF
+sshpass -p "lilama1996" ssh -t  -o StrictHostKeyChecking=no root@104.199.242.108 <<EOF
   pkill -f "java -jar /home/infostaff/java/infostaff-0.0.1.jar"
   pkill -f "java -jar infostaff-0.0.1.jar"
   sleep 1
